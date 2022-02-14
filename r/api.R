af544e369acfe1849dec9d9536f6c8cdfb101e0a
hdolinh <halina@bren.ucsb.edu>
2022-02-14 02:07:21 +0000

    successfully accessed ridb api - activities

--- r/api.R
+++ r/api.R
@@ -1,17 +1,20 @@
 # library(ridbAPI) cannot use because not robust enough and it's a third party package so not sure how reliable that is
 # https://ridb.recreation.gov/api/v1/activities?limit=50&offset=0&apikey=
 
+## SETUP ##
 library(httr)
 library(jsonlite)
 library(tidyverse)
+source("private/ridb-api.R")
 
-api_txt <- "../../Desktop/private/ridb_api.txt"
-
+# activities from RIDB api
 response <- httr::GET("https://ridb.recreation.gov/api/v1/activities",
-          query = list(limit = 5, 
-                       offset = 0, 
-                       apikey = "api_txt"))
+                      query = list(limit = 5, 
+                                   offset = 0, 
+                                   apikey = ridb_api_key))
 
+# check content of the response
+# confirmed it's in json format
 content(response, "text")
 
 fromJSON()
