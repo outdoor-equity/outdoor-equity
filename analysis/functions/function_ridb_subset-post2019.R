06b79881bd0244945e6b8abb7468f6e00aac9312
Clarissa Boyajian <cboyajian@bren.ucsb.edu>
2022-02-18 21:48:39 +0000

    updated to ridb subset functions (change order, comment better)

--- analysis/functions/function_ridb_subset-post2019.R
+++ analysis/functions/function_ridb_subset-post2019.R
@@ -1,46 +1,43 @@
 
 RIDB_subset_post2019 <- function(full_file_path, state_full_name, year) {
-  # variables to keep
-  select_columns <- c("agency", 
-                      "regiondescription", 
-                      "parentlocation",
-                      "park", 
-                      "sitetype", 
-                      "usetype", 
-                      "facilitystate", 
-                      "facilitylongitude", 
-                      "facilitylatitude", 
-                      "customerzip", 
-                      "totalpaid", 
-                      "startdate", 
-                      "enddate", 
-                      "orderdate", 
-                      "numberofpeople")
-  # site types to remove
-  rm_sitetype <- c("historic tour",
-                   "hiking zone",
-                   "group picnic area",
-                   "cave tour",
-                   "management",
-                   "anchorage",
-                   "picnic",
-                   "entry point",
-                   "trailhead")
   # read in file
   ridb_data <- read_csv(here::here(full_file_path))
   # clean data
   df <- ridb_data %>% 
     janitor::clean_names() %>% 
-    select(select_columns) %>% 
+    # filter to state
     filter(facilitystate == state_full_name) %>% 
+    # select variables
+    select(c("agency", 
+             "regiondescription", 
+             "parentlocation",
+             "park", 
+             "sitetype", 
+             "usetype", 
+             "facilitystate", 
+             "facilitylongitude", 
+             "facilitylatitude", 
+             "customerzip", 
+             "totalpaid", 
+             "startdate", 
+             "enddate", 
+             "orderdate", 
+             "numberofpeople")) %>% 
     mutate(sitetype = tolower(sitetype)) %>% 
-    filter(!sitetype %in% rm_sitetype) %>% 
+    filter(!sitetype %in% c("historic tour",
+                            "hiking zone",
+                            "group picnic area",
+                            "cave tour",
+                            "management",
+                            "anchorage",
+                            "picnic",
+                            "entry point",
+                            "trailhead")) %>% 
     filter(usetype == "Overnight") %>%
-    filter(customerzip == str_extract_all(customerzip, "[[:digit:]]{5}")) 
-  # remove use type column
-  df <- 
-    df %>% 
+    filter(customerzip == str_extract_all(customerzip, "[[:digit:]]{5}")) %>% 
+    # remove use type column
     select(!usetype)
+  
   # create df
   assign(paste("data_ridb_", year), data.frame(df), envir = .GlobalEnv)
 }
--- analysis/functions/function_ridb_subset-pre2018.R
+++ analysis/functions/function_ridb_subset-pre2018.R
@@ -1,46 +1,44 @@
 
 RIDB_subset_pre2018 <- function(full_file_path, state_abbrev, year) {
-  # variables to keep
-  select_columns <- c("agency",
-                      "region_description",
-                      "parent_location",
-                      "park",
-                      "site_type", 
-                      "use_type",
-                      "facility_state", 
-                      "facility_longitude", 
-                      "facility_latitude",
-                      "customer_zip", 
-                      "total_paid",
-                      "start_date",
-                      "end_date", 
-                      "order_date",
-                      "number_of_people")
-  # site types to remove
-  rm_sitetype <- c("historic tour",
-                   "hiking zone",
-                   "group picnic area",
-                   "cave tour",
-                   "management",
-                   "anchorage",
-                   "picnic",
-                   "entry point",
-                   "trailhead")
   # read in file
   ridb_data <- read_csv(here::here(full_file_path))
   # clean data
   df <- ridb_data %>% 
     janitor::clean_names() %>% 
-    select(select_columns) %>% 
-    filter(facility_state == state_abbrev) %>% 
+    # filter for state
+    filter(facility_state == state_abbrev) %>%
+    # select variables
+    select(c("agency",
+             "region_description",
+             "parent_location",
+             "park",
+             "site_type", 
+             "use_type",
+             "facility_state", 
+             "facility_longitude", 
+             "facility_latitude",
+             "customer_zip", 
+             "total_paid",
+             "start_date",
+             "end_date", 
+             "order_date",
+             "number_of_people")) %>% 
     mutate(site_type = tolower(site_type)) %>% 
-    filter(!site_type %in% rm_sitetype) %>% 
+    filter(!site_type %in% c("historic tour",
+                             "hiking zone",
+                             "group picnic area",
+                             "cave tour",
+                             "management",
+                             "anchorage",
+                             "picnic",
+                             "entry point",
+                             "trailhead")) %>% 
     filter(use_type == "Overnight") %>%
-    filter(customer_zip == str_extract_all(customer_zip, "[[:digit:]]{5}"))
-  # remove use type column
-  df <- 
-    df %>% 
+    # filter out invalid ZIP codes
+    filter(customer_zip == str_extract_all(customer_zip, "[[:digit:]]{5}")) %>% 
+    # remove use type column
     select(!use_type)
+  
   # create df
   assign(paste0("data_ridb_", year), data.frame(df), envir = .GlobalEnv)
 }
