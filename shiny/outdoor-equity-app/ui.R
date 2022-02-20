a9a43360be4670c872d402d93edabe0e0226e1d8
hdolinh <halina@bren.ucsb.edu>
2022-02-20 04:07:15 +0000

    added theme; created nav bar menu for About tab

--- shiny/outdoor-equity-app/ui.R
+++ shiny/outdoor-equity-app/ui.R
@@ -2,6 +2,7 @@
 ui <- fluidPage(
   
   # set theme
+  theme = bs_theme(bootswatch = "minty"),
   
   # app title ----
   tags$h1("Outdoor Equity App"),
@@ -14,14 +15,16 @@ ui <- fluidPage(
     # title of nav bar
     "Outdoor Equity",
     # nav bar tabs
-    tabPanel("Background and User Guide",
-             "background information and user guide documentation here"),
+    navbarMenu("About",
+               tabPanel("background information"),
+               tabPanel("user guide documentation"),
+               tabPanel("metadata here")),
+    tabPanel("Agencies",
+             "graphs and inputs to compare agenices here"),
+    tabPanel("Reservable Sites",
+             "graphs and inputs to compare reservable sites here"),
     tabPanel("Spatial Analysis",
              "spatial analysis maps and inputs here"),
-    tabPanel("Agency Comparisons",
-             "graphs and inputs to compare agenices here"),
-    tabPanel("Reservable Site Comparisons",
-             "graphs and inputs to compare reservable sites here"),
     tabPanel("Temporal Analysis",
              "graphs that show interesting temporal trends")
   )
