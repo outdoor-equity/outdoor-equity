c8648aad74c522f790ecccebd74666f2f62cb749
hdolinh <halina@bren.ucsb.edu>
2022-04-02 01:15:09 +0000

    created admin unit function

--- shiny/outdoor-equity-app/oe_app/r/inputs.R
+++ shiny/outdoor-equity-app/oe_app/r/inputs.R
@@ -2,7 +2,6 @@
 # used in (1) distribution / single variable analysis; 
 # (2) comparison / multiple variable analysis; 
 # (3) site analysis / maps
-
 select_agency <- function(){
   
   selectizeInput(inputId = "agency",
@@ -15,5 +14,24 @@ select_agency <- function(){
                    onInitialize = I('function() { this.setValue(""); }')
                  )) 
 }
+
+
+# select admin unit ----
+# used in (1) distribution / single variable analysis; 
+# (2) comparison / multiple variable analysis; 
+# (3) site analysis / maps
+select_admin_unit <- function(){
+  
+  selectizeInput(inputId = "admin_unit",
+                 label = "Select an administrative unit",
+                 choices = admin_units,
+                 multiple = TRUE,
+                 options = list(
+                   placeholder = "Type to search for an admin unit",
+                   # Note(HD) when created set a value for the input to an empty string
+                   onInitialize = I('function() { this.setValue(""); }')
+                 )) 
+}
+
 
 
