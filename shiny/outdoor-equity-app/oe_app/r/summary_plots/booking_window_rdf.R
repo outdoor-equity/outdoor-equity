78734a13dd43bb09971bfd30d65cb8d8cca4de05
hdolinh <halina@bren.ucsb.edu>
2022-04-14 06:48:15 +0000

    created booking window rdf function

--- shiny/outdoor-equity-app/oe_app/r/summary_plots/booking_window_rdf.R
+++ shiny/outdoor-equity-app/oe_app/r/summary_plots/booking_window_rdf.R
@@ -1,15 +1,15 @@
 # booking window rdf and wrangling
-booking_window_rdf <- function(agencyId = input$agency_summary, 
-                               admin_unitId = input$admin_unit_summary, 
-                               parkId = input$site_summary){
-  
+# used in REACTIVE DATA FRAMES in server
+# using input id's for summary page in ui
+
+booking_window_rdf <- function(){
   
   data_joined_2018 %>%
-    filter(agency %in% agencyId,
-           admin_unit %in% admin_unitId,
-           park %in% parkId) #%>%
-  # need to add more
-  
-  
+    filter(agency %in% input$agency_summary,
+           admin_unit %in% input$admin_unit_summary,
+           park %in% input$site_summary) %>%
+    filter(booking_window > 0,
+           booking_window < 510) %>% 
+    select(agency, admin_unit, park, booking_window)
   
 } # EO function
