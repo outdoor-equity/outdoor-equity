a03c0dc3f27bc155a8cf45058695d787fe8daaad
hdolinh <halina@bren.ucsb.edu>
2022-04-14 06:48:48 +0000

    edited dist travel rdf function

--- shiny/outdoor-equity-app/oe_app/r/summary_plots/dist_travel_rdf.R
+++ shiny/outdoor-equity-app/oe_app/r/summary_plots/dist_travel_rdf.R
@@ -1,19 +1,13 @@
 # distance traveled rdf and wrangling
 # used in REACTIVE DATA FRAMES in server
 # using input id's for summary page in ui
+dist_travel_rdf <- function(){
 
-dist_travel_rdf <- function(agencyId = input$agency_summary, 
-                               admin_unitId = input$admin_unit_summary, 
-                               parkId = input$site_summary){
-  
-  
   data_joined_2018 %>%
-    filter(agency %in% agencyId,
-           admin_unit %in% admin_unitId,
-           park %in% parkId) %>%
+    filter(agency %in% input$agency_summary,
+           admin_unit %in% input$admin_unit_summary,
+           park %in% input$site_summary) %>%
     mutate(distance_traveled_mi = distance_traveled_m * 0.000621371) %>% 
     select(agency, admin_unit, park, distance_traveled_mi)
-  
-  
   
 } # EO function
