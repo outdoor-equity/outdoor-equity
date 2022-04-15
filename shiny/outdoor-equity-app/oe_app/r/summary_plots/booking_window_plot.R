18780c9104253446ef5aa240b05ffb981962d2dc
hdolinh <halina@bren.ucsb.edu>
2022-04-15 02:51:20 +0000

    added titleInput argument

--- shiny/outdoor-equity-app/oe_app/r/summary_plots/booking_window_plot.R
+++ shiny/outdoor-equity-app/oe_app/r/summary_plots/booking_window_plot.R
@@ -1,20 +1,32 @@
-# booking window plot and parameters
+## booking window plot and parameters ##
 # used in DATA SUMMARY PLOTS in server
 # using input id's for summary page in ui
 
-booking_window_plot <- function(){
+booking_window_plot <- function(agencyInput, admin_unitInput, siteInput, titleInput){
   
+  # reactive data frame
+  booking_window_rdf <- reactive({
+    
+    data_joined_2018 %>%
+      filter(agency %in% agencyInput,
+             admin_unit %in% admin_unitInput,
+             park %in% siteInput) %>%
+      filter(booking_window > 0,
+             booking_window < 510) %>% 
+      select(agency, admin_unit, park, booking_window)
+  })
+    
   # parameters
   hist_colors <- c("#009900FF")
   
   # plot for shiny app
-  ggplot(data = booking_window_df()) +
+  ggplot(data = booking_window_rdf()) +
     geom_histogram(aes(x = booking_window),
                    binwidth = 7,
                    fill = hist_colors) +
     labs(x = "Days elapsed from order to visit (each bar = 1 week)",
          y = "",
-         title = paste("Distribution of Booking Windows for", input$site_summary),
+         title = paste("Distribution of Booking Windows for", titleInput),
          subtitle = "Overnight Reservations in California in 2018") +
     scale_x_continuous(limits = c(0, 510), 
                        breaks = seq(0, 510, by = 30)) +
