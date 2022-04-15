3e75dbd0eb407fa3b0b517f5bda1d9570c9ae6ef
hdolinh <halina@bren.ucsb.edu>
2022-04-15 04:25:38 +0000

    added race, education, and median income plots

--- shiny/outdoor-equity-app/oe_app/server.R
+++ shiny/outdoor-equity-app/oe_app/server.R
@@ -248,6 +248,36 @@ output$data_summary_plot <- renderPlot({
     
   } ## EO else if site type
   
+  ## SO race ----
+  else if (input$data_summary == "race") {
+    
+    race_plot(agencyInput = input$agency_summary,
+              admin_unitInput = input$admin_unit_summary,
+              siteInput = input$site_summary,
+              titleInput = input$site_summary)
+    
+  } ## EO else if race
+  
+  ## SO education ----
+  else if (input$data_summary == "education") {
+    
+    education_plot(agencyInput = input$agency_summary,
+                   admin_unitInput = input$admin_unit_summary,
+                   siteInput = input$site_summary,
+                   titleInput = input$site_summary)
+    
+  } ## EO else if education
+  
+  ## SO median income ----
+  else if (input$data_summary == "median_income") {
+    
+    median_income_plot(agencyInput = input$agency_summary,
+                       admin_unitInput = input$admin_unit_summary,
+                       siteInput = input$site_summary,
+                       titleInput = input$site_summary)
+    
+  } ## EO else if median income
+  
   
 }) ## EO data summary plots
 
