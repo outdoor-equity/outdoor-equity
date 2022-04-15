c4d491cf592fd8fe40cf7a7f3551e69b4e67480f
hdolinh <halina@bren.ucsb.edu>
2022-04-15 03:33:49 +0000

    added daily cost per visitor, length of stay, site type functions

--- shiny/outdoor-equity-app/oe_app/server.R
+++ shiny/outdoor-equity-app/oe_app/server.R
@@ -216,7 +216,37 @@ output$data_summary_plot <- renderPlot({
                         siteInput = input$site_summary,
                         titleInput = input$site_summary)
     
-  } ### EO else if booking window
+  } ## EO else if booking window
+  
+  ## SO daily cost per visitor ----
+  else if (input$data_summary == "daily_cost_per_visitor") {
+    
+    daily_cost_visitor_plot(agencyInput = input$agency_summary,
+                        admin_unitInput = input$admin_unit_summary,
+                        siteInput = input$site_summary,
+                        titleInput = input$site_summary)
+    
+  } ## EO else if daily cost per visitor
+  
+  ## SO length of stay ----
+  else if (input$data_summary == "length_of_stay") {
+    
+    length_of_stay_plot(agencyInput = input$agency_summary,
+                        admin_unitInput = input$admin_unit_summary,
+                        siteInput = input$site_summary,
+                        titleInput = input$site_summary)
+    
+  } ## EO else if length of stay
+  
+  ## SO site type ----
+  else if (input$data_summary == "aggregated_site_type") {
+    
+    site_type_plot(agencyInput = input$agency_summary,
+                   admin_unitInput = input$admin_unit_summary,
+                   siteInput = input$site_summary,
+                   titleInput = input$site_summary)
+    
+  } ## EO else if site type
   
   
 }) ## EO data summary plots
