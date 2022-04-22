733de8ddc7772d40529f7ed5e103ef6a106d004b
hdolinh <halina@bren.ucsb.edu>
2022-04-22 21:18:11 +0000

    sourced oe admin unit to site function

--- shiny/outdoor-equity-app/oe_app/global.R
+++ shiny/outdoor-equity-app/oe_app/global.R
@@ -56,6 +56,7 @@ source("r/summary_plots/median_income_plot.R")
 
 ## source observe event functions ----
 source("r/inputs/observe_event_agency_to_admin_dict.R")
+source("r/inputs/observe_event_admin_unit_to_site_dict.R")
 
 # need to move these sourced functions to a new section
 source("r/summary_plots/tot_site_agency_plot.R")
