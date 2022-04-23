f3a62adf2f4dfaba656ae9ae5c1cf93e0871dadb
Clarissa Boyajian <cboyajian@bren.ucsb.edu>
2022-04-23 01:23:49 +0000

    create functions for race x distance plot in shiny

--- shiny/outdoor-equity-app/oe_app/r/relationship_plots/race_dist_travel_plot.R
+++ shiny/outdoor-equity-app/oe_app/r/relationship_plots/race_dist_travel_plot.R
@@ -1,53 +1,44 @@
 
 ## race x distance traveled and parameters ##
 
-dist_travel_plot <- function(agencyInput, admin_unitInput, siteInput, 
-                             race_group, 
-                             acs_df = data_ca_acs_2018, 
-                             ridb_df = data_ridb_acs_2018){
+dist_travel_plot <- function(race_top_quartile_df){
   
-  ## NEED TO ADD REACTIVE DF ELEMENTS STILL
-
-    df_ca_bin_breaks <- acs_df %>%
-      select(zip_code, asian, black, hispanic_latinx, multiracial,
-             native_american, other, pacific_islander, white, mean_zip_code_population) %>%
-      pivot_longer(cols = 2:9,
-                   names_to = "race",
-                   values_to = "race_percentage") %>%
-      filter(race == race_group) %>% ## BACK TO i
-      drop_na(race_percentage)
-    
-    weighted_half <- weighted.mean(x = df_ca_bin_breaks$race_percentage, w = df_ca_bin_breaks$mean_zip_code_population)
-    
-    df_ca_bin_breaks <- df_ca_bin_breaks %>% filter(race_percentage >= weighted_half)
-    
-    weighted_quartile <- weighted.mean(x = df_ca_bin_breaks$race_percentage, w = df_ca_bin_breaks$mean_zip_code_population)
-    
-    df_racial_group_i_longer <- data_ridb_acs_2018 %>% 
-      select(agency, admin_unit, park, customer_zip, asian, black, hispanic_latinx, 
-             multiracial, native_american, other, pacific_islander, white,
-             distance_traveled_m) %>% 
-      mutate(distance_traveled_mi = distance_traveled_m * 0.000621371) %>% 
-      drop_na(distance_traveled_mi) %>% 
-      pivot_longer(cols = 5:12,
-                   names_to = "race",
-                   values_to = "race_percentage") %>% 
-      filter(race == race_group) %>% ## BACK TO i
-      drop_na(race_percentage)
-    
-    max_racial_group_ridb <- df_racial_group_i_longer %>%
-      summarize(max = max(race_percentage))
-    
-    df_racial_group_i <- df_racial_group_i_longer %>%
-      filter(race_percentage >= weighted_quartile) %>% 
-      summarize(mean_distance_traveled_mi = mean(distance_traveled_mi)) %>% 
-      mutate(race = paste0(race_group)) %>%  ## BACK TO i
-      relocate(race, .before = 1) %>% 
-      mutate(race = str_replace(string = race,
-                                pattern = "_",
-                                replacement = " "),
-             race = str_to_title(race),
-             race = str_replace(string = race,
-                                pattern = "Other",
-                                replacement = "Other Race(s)"))
+  data_plot_race_distanceTravel <- 
+    race_top_quartile_df %>% pmap_dfr(function_plot_race_distanceTravel)
+  
+  # create plot
+  race_group_colors <- c("Other Race(s)" = "#999999", "Pacific Islander" = "#E69F00", "Multiracial" = "#56B4E9",
+                         "Asian" = "#009E73", "Black" = "#F0E442", "White" = "#0072B2", 
+                         "Native American" = "#D55E00", "Hispanic Latinx" = "#CC79A7")
+  
+  plot_race_distance_traveled <- ggplot(data = data_plot_race_distanceTravel, 
+                                        aes(x = mean_distance_traveled_mi,
+                                            y = reorder(race, mean_distance_traveled_mi))) +
+    geom_segment(aes(xend = 0, yend = race)) +
+    geom_point(aes(x = mean_distance_traveled_mi,
+                   y = reorder(race, mean_distance_traveled_mi),
+                   color = race, fill = race,
+                   text = paste("People who live in ZIP codes with high", 
+                                str_to_title(race) %>% 
+                                  str_replace(string = ., pattern = "\\(S\\)", "\\(s\\)"), 
+                                "populations", "<br>travel an estimated distance of", 
+                                round(mean_distance_traveled_mi, 0), 
+                                "miles to overnight reservable sites.")),
+               width = .25,
+               size = 4, shape = 21, stroke = 2) +
+    scale_fill_manual(values = race_group_colors) +
+    scale_color_manual(values = race_group_colors) +
+    labs(x = paste("Average Distance Traveled from Home to Site (miles)"),
+         y = "",
+         title = "Average Distance Traveled to Reservation for <br>Different Racial Groups") + 
+    theme_minimal() +
+    theme(plot.background = element_rect("white"),
+          panel.grid.major.y = element_blank(),
+          legend.position = "none")
+  
+  ggplotly(plot_race_distance_traveled,
+           tooltip = list("text"))   
+  
+  return(df_racial_group_i)
+  
 } # EO function
