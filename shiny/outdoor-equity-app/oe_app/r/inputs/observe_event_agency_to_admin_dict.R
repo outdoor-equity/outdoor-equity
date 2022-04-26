292cd58fe05e46d0beb284b0197634441242cfaf
hdolinh <halina@bren.ucsb.edu>
2022-04-26 21:56:14 +0000

    tried creating a wrapper function for OE; unsuccessful went back to old function

--- shiny/outdoor-equity-app/oe_app/r/inputs/observe_event_agency_to_admin_dict.R
+++ shiny/outdoor-equity-app/oe_app/r/inputs/observe_event_agency_to_admin_dict.R
@@ -3,7 +3,7 @@
 # using input id's for all analysis and data download pages in ui
 
 oe_agency_to_admin_dict <- function(isInput_key, page){
-
+  
   print(paste0("You have chosen agency: ", isInput_key, " on the ", page, " page and the class is: ", class(isInput_key)))
   
   # create choices based on dictionary
