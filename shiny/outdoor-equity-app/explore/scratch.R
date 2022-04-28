85cf980e006d8e7d22c6a7d36e6dd1ed0955f625
hdolinh <halina@bren.ucsb.edu>
2022-04-28 20:47:22 +0000

    testing daily cost per visitor function

--- shiny/outdoor-equity-app/explore/scratch.R
+++ shiny/outdoor-equity-app/explore/scratch.R
@@ -11,7 +11,55 @@ booking_window_rdf <- data_joined_2018 %>%
 x_max <- (round(max(booking_window_rdf$booking_window) / 5) * 5) + 5
 
 
+daily_cost_visitor <- data_joined_2018 %>%
+  filter(park %in% "Anacapa Island") %>%
+  filter(daily_cost_per_visitor != "Inf") %>%
+  select(park, daily_cost_per_visitor)
 
+x_max_test <- (round(max(daily_cost_visitor$daily_cost_per_visitor) / 5) * 5) + 5
+
+quant_80 <- quantile(x = daily_cost_visitor$daily_cost_per_visitor,
+                     probs = seq(0, 1, 0.1))[[9]] %>% round(0)
+
+hist_colors <- c("#009900FF", "#00c000")
+
+# plot for shiny app
+daily_cost_plotly <- ggplot(data = daily_cost_visitor) +
+  geom_histogram(aes(x = daily_cost_per_visitor, 
+                     text = paste(percent(..count.. / nrow(daily_cost_visitor), accuracy = 0.1), 
+                                  "of all reservations paid between", dollar(xmin), "and", dollar(xmax),
+                                  "<br>(All reservations to site: ",
+                                  comma(nrow(daily_cost_visitor), accuracy = 1), ")")),
+                 binwidth = 1,
+                 center = 0.5,
+                 fill = hist_colors[[1]], 
+                 col = hist_colors[[2]], size = 0.05) +
+  labs(x = "Daily cost per visitor ($)",
+       y = "",
+       title = paste0("Daily Cost per Visitor for Visits to")) +
+  scale_x_continuous(limits = c(0, x_max), labels = dollar) +
+  scale_y_continuous(labels = comma) +
+  geom_vline(xintercept = quant_80,
+             linetype = "dashed", alpha = 0.5, color = "#000099") +
+  theme_minimal() +
+  theme(plot.background = element_rect("white"),
+        panel.grid.major.y = element_blank(),
+        plot.title = element_text(size = 11))
+
+ggplotly(daily_cost_plotly,
+         tooltip = list("text"),
+         dynamicTicks = TRUE) %>% 
+  layout(margin = list(b = 130, t = 100), 
+         annotations =  list(x = 1, 
+                             y = -0.6, 
+                             text = paste0("80% of reservations paid less than ", dollar(quant_80), 
+                                           " per visitor per day <br>(shown on plot with dotted line)."), 
+                             showarrow = F, 
+                             xre = 'paper', yref = 'paper', 
+                             xanchor = 'left', 
+                             yanchor = 'auto', 
+                             xshift = 0, yshift = 0,
+                             font = list(size = 12, color = "#000099")))
 
 
 dist_travel_df <- data_joined_2018 %>%
