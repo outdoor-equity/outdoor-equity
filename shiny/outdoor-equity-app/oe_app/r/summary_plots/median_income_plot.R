6116857bc494b6bb6ea24426376fe6d3fc4967c9
Clarissa Boyajian <cboyajian@bren.ucsb.edu>
2022-04-28 19:40:46 +0000

    updates to median income function

--- shiny/outdoor-equity-app/oe_app/r/summary_plots/median_income_plot.R
+++ shiny/outdoor-equity-app/oe_app/r/summary_plots/median_income_plot.R
@@ -2,17 +2,15 @@
 # used in DATA SUMMARY PLOTS in server
 # using input id's for summary page in ui
 
-median_income_plot <- function(agencyInput, admin_unitInput, siteInput, titleInput){
+median_income_plot <- function(admin_unitInput, siteInput){
   
   # reactive data frame 
   median_income_rdf <- reactive ({
     
     # reservations in CA
     data_joined_2018 %>%
-      filter(agency %in% agencyInput,
-             admin_unit %in% admin_unitInput,
-             park %in% siteInput) %>%
-      select(agency, admin_unit, park, customer_zip, median_income)
+      filter(park %in% siteInput) %>%
+      select(park, customer_zip, median_income)
     
   }) # EO RDF
   
@@ -20,6 +18,8 @@ median_income_plot <- function(agencyInput, admin_unitInput, siteInput, titleInp
   # CA population
   data_plot_median_income_ca <- data_ca_acs_2018 %>%
     select(zip_code, median_income)
+  
+  x_max <- max(median_income_rdf()$education_percent_average) + 0.1 # max x rounded to nearest 5
   
   # parameters
   groups_colors_ridb_ca <- c("RIDB" = "#009900FF", "CA" = "#666666")
@@ -39,16 +39,15 @@ median_income_plot <- function(agencyInput, admin_unitInput, siteInput, titleInp
                        bins = 10), 
                    fill = groups_colors_ridb_ca[[1]], 
                    alpha = 0.75) +
+    scale_x_continuous(limits = c(0, x_max), labels = comma) +
+    scale_y_continuous(labels = percent) +
     geom_vline(xintercept = 75277, 
                linetype = "dashed", size = .3, alpha = .5) +
     annotate("text", label = "Median household income \nin 2018 in CA ($75,277)", 
              x = 118000, y = .15) +
     labs(x = "Median Income ($))",
          y = "",
-         title = paste("Distribution of Median Income in Home in ZIP Codes in 2018 for", titleInput),
-         subtitle = "Visitors' home ZIP codes for Overnight Reservations in California \nvs. California Residents") +
-    scale_x_continuous(limits = c(0, 260000), breaks = seq(0, 260000, 50000), minor_breaks = seq(0, 260000, 25000), labels = comma) +
-    scale_y_continuous(labels = scales::percent) +
+         title = paste0("Median Income in Home in ZIP Codes in 2018 for ", siteInput, ", ", admin_unitInput)) +
     theme_minimal() +
     theme(plot.background = element_rect("white"),
           panel.grid.major.y = element_blank())
