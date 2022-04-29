5245ab8451570f5857ad2a36e37248f2b268a555
Clarissa Boyajian <cboyajian@bren.ucsb.edu>
2022-04-29 22:02:30 +0000

    created template scripts for relationships functions

---
+++ shiny/outdoor-equity-app/oe_app/r/relationship_plots/template_data.R
@@ -0,0 +1,43 @@
+
+race_dist_travel_data <- function(agencyInput, admin_unitInput, siteInput, 
+                                  race_group, weighted_quartile,
+                                  ridb_df){
+  # reactive data frame 
+  race_dist_travel_rdf <- reactive ({
+    
+    ridb_df %>%
+      filter(agency %in% agencyInput,
+             admin_unit %in% admin_unitInput,
+             park %in% siteInput) %>%
+      select(agency, admin_unit, park, customer_zip, asian, black, hispanic_latinx, 
+             multiracial, native_american, other, pacific_islander, white,
+             distance_traveled_m) %>% 
+      mutate(distance_traveled_mi = distance_traveled_m * 0.000621371) %>% 
+      drop_na(distance_traveled_mi) %>% 
+      pivot_longer(cols = 5:12,
+                   names_to = "race",
+                   values_to = "race_percentage") %>% 
+      filter(race == paste0(race_group)) %>% 
+      drop_na(race_percentage)
+  })
+  
+  
+  max_racial_group_ridb <- race_dist_travel_rdf() %>%
+    summarize(max = max(race_percentage))
+  
+  df_racial_group_i <- race_dist_travel_rdf() %>%
+    filter(race_percentage >= weighted_quartile) %>% 
+    summarize(mean_distance_traveled_mi = mean(distance_traveled_mi)) %>% 
+    mutate(race = paste0(race_group)) %>% 
+    relocate(race, .before = 1) %>% 
+    mutate(race = str_replace(string = race,
+                              pattern = "_",
+                              replacement = " "),
+           race = str_to_title(race),
+           race = str_replace(string = race,
+                              pattern = "Other",
+                              replacement = "Other Race(s)"))
+  
+  return(df_racial_group_i)
+  
+} # EO function
