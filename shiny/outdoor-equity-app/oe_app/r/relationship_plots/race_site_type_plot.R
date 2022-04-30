0d5eeb037b34402e881e37badedd13572c13ff57
Clarissa Boyajian <cboyajian@bren.ucsb.edu>
2022-04-30 00:42:33 +0000

    updated object names to match summary plotly function conventions

--- shiny/outdoor-equity-app/oe_app/r/relationship_plots/race_site_type_plot.R
+++ shiny/outdoor-equity-app/oe_app/r/relationship_plots/race_site_type_plot.R
@@ -14,17 +14,17 @@
 #' @export
 #'
 #' @examples
-race_dist_travel_plot <- function(admin_unitInput, siteInput,
+race_site_type_plot <- function(admin_unitInput, siteInput,
                                   race_top_quartile_df, ridb_df,
                                   site_type_string){
   
-  print(race_dist_travel_data)
+  print(race_site_type_plot)
   print(ridb_df)
   print(admin_unitInput)
   print(siteInput)
   
   # iterate to create df for plotting
-  data_plot_race_distanceTravel <- 
+  data_race_site_type <- 
     race_top_quartile_df %>% pmap_dfr(race_site_type_data, 
                                       ridb_df = ridb_df, 
                                       siteInput = siteInput) %>% 
@@ -34,7 +34,7 @@ race_dist_travel_plot <- function(admin_unitInput, siteInput,
                                               pattern = "Rv", 
                                               replacement = "RV"))
   
-  print(head(data_plot_race_distanceTravel))
+  print(head(data_race_site_type))
   
   ## -- create plot -- ##
   # parameters
@@ -44,15 +44,15 @@ race_dist_travel_plot <- function(admin_unitInput, siteInput,
   
   
   # create plot (or say no such site type if none exist at siteInput)
-  if (nrow(data_plot_race_distanceTravel) == 0){
+  if (nrow(data_race_site_type) == 0){
     
     print(paste0("There are no ", str_to_title(site_type_string) %>% 
                    str_replace(string = ., pattern = "Rv", replacement = "RV"), " 
                  sites at ", siteInput, ", ", admin_unitInput, "."))
     
-  } else if (nrow(data_plot_race_distanceTravel) > 0){
+  } else if (nrow(data_race_site_type) > 0){
     
-    plot_race_distance_traveled <- ggplot(data = data_plot_race_distanceTravel) +
+    race_site_type_plotly <- ggplot(data = data_race_site_type) +
       geom_col(aes(x = n,
                    y = reorder(race, n),
                    fill = race,
@@ -62,12 +62,12 @@ race_dist_travel_plot <- function(admin_unitInput, siteInput,
                                  race, " populations."))) +
       scale_fill_manual(values = race_group_colors) +
       scale_color_manual(values = race_group_colors) +
-      labs(x = paste("Number of Reservations to", str_to_title(type_string) %>% 
+      labs(x = paste("Number of Reservations to", str_to_title(site_type_string) %>% 
                        str_replace(string = ., pattern = "Rv", replacement = "RV"), 
                      "Overnight Reservable Sites"),
            y = "",
            title = paste0("Number of Reservations to ", 
-                          str_to_title(type_string) %>% 
+                          str_to_title(site_type_string) %>% 
                             str_replace(string = ., pattern = "Rv", replacement = "RV"),
                           " Sites by <br>Different Racial Groups")) + 
       theme_minimal() +
@@ -75,7 +75,7 @@ race_dist_travel_plot <- function(admin_unitInput, siteInput,
             panel.grid.major.y = element_blank(),
             legend.position = "none")
     
-    ggplotly(plot_race_distance_traveled,
+    ggplotly(race_site_type_plotly,
              tooltip = list("text"))   
   } # EO else if
   
