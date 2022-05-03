3adbc49cd54b0081a326b282d655b1dd357cd9f8
Clarissa Boyajian <cboyajian@bren.ucsb.edu>
2022-05-03 00:56:38 +0000

    created race x booking window plotly funciton

---
+++ shiny/outdoor-equity-app/oe_app/r/relationship_plots/race_booking_window_data.R
@@ -0,0 +1,36 @@
+
+race_dist_travel_data <- function(siteInput, race_group, weighted_quartile, ridb_df){
+  # reactive data frame 
+  race_booking_window_rdf <- reactive ({
+    
+    data_joined_2018 %>%
+      filter(park %in% siteInput) %>%
+      select(park, customer_zip, asian, black, hispanic_latinx, 
+             multiracial, native_american, other, pacific_islander, white,
+             booking_window) %>% 
+      drop_na(booking_window) %>% 
+      pivot_longer(cols = 3:10,
+                   names_to = "race",
+                   values_to = "race_percentage") %>% 
+      filter(race == race_group) %>%
+      drop_na(race_percentage) %>% 
+      filter(race_percentage >= weighted_quartile) %>% 
+      summarize(median_booking_window = median(booking_window),
+                quartile_lower = quantile(booking_window)[[2]],
+                quartile_upper = quantile(booking_window)[[4]],
+                count = n()) %>% 
+      mutate(race = paste0(race_group)) %>%  ## BACK TO i
+      relocate(race, .before = 1) %>% 
+      mutate(race = str_replace(string = race,
+                                pattern = "_",
+                                replacement = " "),
+             race = str_to_title(race),
+             race = str_replace(string = race,
+                                pattern = "Other",
+                                replacement = "Other Race(s)"))
+    
+  })
+  
+  return(race_booking_window_rdf())
+  
+} # EO function
---
+++ shiny/outdoor-equity-app/oe_app/r/relationship_plots/race_booking_window_plot.R
@@ -0,0 +1,71 @@
+
+## race x distance traveled and parameters ##
+
+race_dist_travel_plot <- function(admin_unitInput, siteInput,
+                                  race_top_quartile_df, ridb_df){
+  
+  print(race_dist_travel_data)
+  print(ridb_df)
+  print(admin_unitInput)
+  print(siteInput)
+  
+  data_race_booking_window <- 
+    race_top_quartile_df %>% pmap_dfr(race_booking_window_data, 
+                                      ridb_df = ridb_df, 
+                                      siteInput = siteInput)
+  
+  print(head(data_race_dist_travel))
+  
+  # create plot
+  race_group_colors <- c("Other Race(s)" = "#999999", "Pacific Islander" = "#E69F00", "Multiracial" = "#56B4E9",
+                         "Asian" = "#009E73", "Black" = "#F0E442", "White" = "#0072B2", 
+                         "Native American" = "#D55E00", "Hispanic Latinx" = "#CC79A7")
+  
+  # create plot (or say no such site type if none exist at siteInput)
+  if (nrow(data_race_booking_window) == 0){
+    
+    print(paste0("There are no reservations to ", siteInput, ", ", admin_unitInput, 
+                 " that come from communities that fall into the high range for any racial groups."))
+    
+  } else if (nrow(data_race_booking_window) > 0){
+    
+    race_distance_traveled_plotly <- ggplot(data = data_race_booking_window, 
+                                            aes(x = median_booking_window,
+                                                y = reorder(race, median_booking_window))) +
+      geom_segment(aes(xend = 0, yend = race)) +
+      geom_point(aes(color = race, fill = race,
+                     text = paste0(comma(count, accuracy = 1), 
+                                   " unique visits were made by people who live in ZIP codes<br>with high ",
+                                   str_to_title(race) %>% 
+                                     str_replace(string = ., pattern = "\\(S\\)", "\\(s\\)"), 
+                                   " populations. Typically folks reserved their visit between<br>",
+                                   comma(quartile_lower, accuracy = 1), 
+                                   " and ", comma(quartile_upper, accuracy = 1), 
+                                   " days before the start of their trip, with a median booking window of ", 
+                                   comma(median_booking_window, accuracy = 1), 
+                                   " days.")),
+                 size = 3.5, 
+                 shape = 21, stroke = 2) +
+      scale_y_discrete(expand = c(0.3, 0)) +
+      scale_fill_manual(values = race_group_colors) +
+      scale_color_manual(values = race_group_colors) +
+      labs(x = paste("Estimated Number of Days in Advance Site is Reserved (days)"),
+           y = "",
+           title = paste0("Number of Days in Advance Site is Reserved by Different Racial Groups to<br>", 
+                          siteInput, ", ", admin_unitInput, " in 2018")) + 
+      theme_minimal() +
+      theme(plot.background = element_rect("white"),
+            panel.grid.major.y = element_blank(),
+            legend.position = "none")
+    
+    ggplotly(race_distance_traveled_plotly,
+             tooltip = list("text")) %>%
+      config(modeBarButtonsToRemove = list("pan", "select", "lasso2d", "autoScale2d", 
+                                           "hoverClosestCartesian", "hoverCompareCartesian")) %>% 
+      add_annotations(text = "Reservations from ZIP codes<br>with high proportions of:", 
+                      x = -0.15, xref = 'paper', y = 0.95, yref = 'paper', 
+                      showarrow = FALSE)
+    
+  } # EO else if
+  
+} # EO function
