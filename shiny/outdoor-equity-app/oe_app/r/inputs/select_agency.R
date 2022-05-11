83e3a625b9b8d8f6c15d43284bbb4230b2e31642
hdolinh <halina@bren.ucsb.edu>
2022-05-11 05:53:46 +0000

    added size parameter

--- shiny/outdoor-equity-app/oe_app/r/inputs/select_agency.R
+++ shiny/outdoor-equity-app/oe_app/r/inputs/select_agency.R
@@ -1,11 +1,12 @@
 # SEL agency ----
 # used in (1) data summary; (2) data relationships; (3) visitorsheds; (4) data download
-select_agency <- function(locationId, isMultiple = FALSE){
+select_agency <- function(locationId, isMultiple = FALSE, isSize = NULL){
   
   selectizeInput(inputId = paste("agency", locationId, sep = "_"),
                  label = "Select an agency",
                  choices = ca_agency,
                  multiple = isMultiple,
+                 size = isSize,
                  options = list(
                    placeholder = "Type to search for an agency",
                    # Note(HD) when created set a value for the input to an empty string
