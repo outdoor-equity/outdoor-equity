60927675e5a422b1689d1b69c94cc959d708ab0e
Clarissa Boyajian <cboyajian@bren.ucsb.edu>
2022-05-12 20:15:13 +0000

    Updated booking window to include 6 month / 1 year annotation for sites with reservations 6 months to 1 year out

--- shiny/outdoor-equity-app/oe_app/r/summary_plots/booking_window_plot.R
+++ shiny/outdoor-equity-app/oe_app/r/summary_plots/booking_window_plot.R
@@ -47,7 +47,9 @@ booking_window_plot <- function(admin_unitInput, siteInput){
   hist_colors <- c("#009900FF", "#00c000")
   
   # plot for shiny app
-  booking_window_plotly <- ggplot(
+  if (x_max <= 180) {
+  # don't include 6 month or 1 year annotation
+    plotly <- ggplot(
     data = booking_window_rdf()) +
     geom_histogram(aes(x = booking_window,
                        text = paste0(percent(..count.. / nrow(booking_window_rdf()), accuracy = 0.1), 
@@ -68,7 +70,67 @@ booking_window_plot <- function(admin_unitInput, siteInput){
     theme(plot.background = element_rect("white"),
           panel.grid.major.y = element_blank())
   
-  ggplotly(booking_window_plotly,
+  } else if (x_max > 180 & x_max <= 360){
+    # include 6 month annotation
+    plotly <- ggplot(
+      data = booking_window_rdf()) +
+      geom_histogram(aes(x = booking_window,
+                         text = paste0(percent(..count.. / nrow(booking_window_rdf()), accuracy = 0.1), 
+                                       " of all visits are reserved between ", xmin, " and ", xmax, 
+                                       " days before the start of the visit", 
+                                       "<br>(All reservations to site: ",
+                                       comma(nrow(booking_window_rdf()), accuracy = 1), ")")),
+                     binwidth = 7,
+                     center = 7 / 2,
+                     fill = hist_colors[[1]], 
+                     col = hist_colors[[2]], size = 0.05) +
+      geom_vline(xintercept = 180, 
+                 linetype = "dashed", size = .3, alpha = .5) +
+      annotate("text", label = "6 months", 
+               x = 210, y = 10) +
+      labs(x = "Days in advance before visit (each bar = 1 week)",
+           y = "") +
+      scale_x_continuous(limits = c(0, x_max)) +
+      geom_vline(xintercept = quant_80,
+                 linetype = "dashed", alpha = 0.5, color = "#000099") +
+      theme_minimal() +
+      theme(plot.background = element_rect("white"),
+            panel.grid.major.y = element_blank())
+    
+  } else if (x_max >= 360) {
+    
+    # include 6 month and 1 year annotation
+    plotly <- ggplot(
+      data = booking_window_rdf()) +
+      geom_histogram(aes(x = booking_window,
+                         text = paste0(percent(..count.. / nrow(booking_window_rdf()), accuracy = 0.1), 
+                                       " of all visits are reserved between ", xmin, " and ", xmax, 
+                                       " days before the start of the visit", 
+                                       "<br>(All reservations to site: ",
+                                       comma(nrow(booking_window_rdf()), accuracy = 1), ")")),
+                     binwidth = 7,
+                     center = 7 / 2,
+                     fill = hist_colors[[1]], 
+                     col = hist_colors[[2]], size = 0.05) +
+      geom_vline(xintercept = 180, 
+                 linetype = "dashed", size = .3, alpha = .5) +
+      annotate("text", label = "6 months", 
+               x = 210, y = 10) +
+      geom_vline(xintercept = 360,
+                 linetype = "dashed", size = .3, alpha = .5) +
+      annotate("text", label = "1 year",
+               x = 380, y = 10) +
+      labs(x = "Days in advance before visit (each bar = 1 week)",
+           y = "") +
+      scale_x_continuous(limits = c(0, x_max)) +
+      geom_vline(xintercept = quant_80,
+                 linetype = "dashed", alpha = 0.5, color = "#000099") +
+      theme_minimal() +
+      theme(plot.background = element_rect("white"),
+            panel.grid.major.y = element_blank())
+  } # EO else if for plotly
+  
+  ggplotly(plotly,
            tooltip = list("text"),
            dynamicTicks = TRUE) %>% 
     layout(title = list(text = paste0('<b>', siteInput, '<br>', admin_unitInput, '</b>',
