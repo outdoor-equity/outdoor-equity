c7cb44bf2ff76731b1422ec385ada44e95c6af84
Clarissa Boyajian <cboyajian@bren.ucsb.edu>
2022-05-18 17:58:39 +0000

    updated state / ZIP geometries for visitorshed to be more simplified

--- analysis/functions/function_map_us_data.R
+++ analysis/functions/function_map_us_data.R
@@ -60,7 +60,7 @@ zip_state_dataframe <- function(year = 2018){
     rename(fips = GEOID,
            state_abbrev = STUSPS,
            state = NAME) %>% 
-    rmapshaper::ms_simplify(keep = 0.05, keep_shapes = TRUE) %>% 
+    rmapshaper::ms_simplify(keep = 0.005, keep_shapes = TRUE) %>% 
     left_join(y = df_states_fips_zip_codes,
               by = "fips") %>% 
     relocate(zip_code, .before = 1)
