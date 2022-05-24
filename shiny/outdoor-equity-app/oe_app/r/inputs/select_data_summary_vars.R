54fcc449f501d68d12ed89074508ff953dd8fac3
hdolinh <halina@bren.ucsb.edu>
2022-05-24 00:05:16 +0000

    removed number from label

--- shiny/outdoor-equity-app/oe_app/r/inputs/select_data_summary_vars.R
+++ shiny/outdoor-equity-app/oe_app/r/inputs/select_data_summary_vars.R
@@ -3,7 +3,7 @@
 select_data_summary_vars <- function(){
   
   selectizeInput(inputId = "data_summary",
-                 label = "1. Select a variable",
+                 label = "Select a variable",
                  choices = sort(summary_vars), 
                  multiple = FALSE,
                  options = list(
