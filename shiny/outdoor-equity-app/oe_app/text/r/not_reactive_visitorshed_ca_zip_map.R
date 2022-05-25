2a6b70d4ed025297ac583b4323d82d2b4179b37f
Clarissa Boyajian <cboyajian@bren.ucsb.edu>
2022-05-25 05:08:40 +0000

    added new function for about page explanation to mimic analysis tab better

---
+++ shiny/outdoor-equity-app/oe_app/text/r/not_reactive_visitorshed_ca_zip_map.R
@@ -0,0 +1,78 @@
+
+#' About examples: CA ZIP Code Visitorshed Map
+#'
+#' @param siteInput User pick for site
+#' @param ridb_df RIDB dataframe object name
+#' @param zip_geometries_df State geometries dataframe object name
+#' @param ca_cities_df Major CA cities geometries dataframe object name
+#'
+#' @return Interactive map of visitorshed by state
+#'
+#' @examples
+not_reactive_ca_zip_code_visitorshed_map <- function(site, 
+                                        ridb_df, 
+                                        zip_geometries_df, 
+                                        ca_cities_df){
+  
+  ## -- data wrangle -- ##
+  # dataframe of number of reservations per state
+  data <- ridb_df %>% 
+    select(agency, admin_unit, park, customer_zip, 
+           facility_latitude, facility_longitude) %>% 
+    filter(park %in% site)
+  
+  map_data <- data %>% 
+    group_by(customer_zip) %>%
+    summarize(number_reservations = n())
+  
+  # add geometries
+  map_data_geometries <-
+    zip_geometries_df %>% 
+    left_join(map_data,
+              by = c("zip_code" = "customer_zip")) %>%
+    mutate_at(vars(number_reservations), 
+              ~replace(number_reservations, is.na(number_reservations), 0))  %>% 
+    select(zip_code, number_reservations, geometry)
+  
+  # value of total CA reservations for this park
+  total_reservations <- sum(map_data_geometries$number_reservations)
+  
+  # add percentage of all CA reservations for each ZIP
+  map_data_geometries <- map_data_geometries %>% 
+    mutate(percentage_reservations = percent((number_reservations / total_reservations), accuracy = 0.01)) %>%
+    mutate_at(vars(percentage_reservations), 
+              ~replace(percentage_reservations, is.na(percentage_reservations), 0))
+  
+  # calculate location of park for point on map
+  park_location_geom <- data %>%
+    group_by(park) %>%
+    summarise(facility_latitude = median(facility_latitude),
+              facility_longitude = median(facility_longitude)) %>%
+    st_as_sf(coords = c("facility_latitude", "facility_longitude"),
+             crs = 4326) %>%
+    st_transform(crs = 4269) # using NAD83 because measured in meters
+  
+  
+  ## -- create map -- ##
+  tmap_mode("view")
+  
+  tm_shape(map_data_geometries) +
+    tm_fill(col = "number_reservations",
+            title = "Number of Visits",
+            palette = "PuRd",
+            style = "jenks",
+            n = 10,
+            popup.vars = c("Total Visits" = "number_reservations",
+                           "Percentage of All CA Visits" = "percentage_reservations")) +
+    tm_shape(park_location_geom) +
+    tm_dots(col = "darkgreen", size = 0.1) +
+    tm_shape(park_location_geom) +
+    tm_symbols(shape = map_site_icon,
+               id = "park") +
+    tm_shape(ca_cities_df) +
+    tm_text(col = "black",
+            text = "city") +
+    tm_view(set.view = c(-119.559917, 37.061753, 6)) +
+    tmap_options(basemaps = 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}')
+  
+}
